09bc40f1c60131c11bdc8289373a7a5f42138da1
Ali Rezaei <alirezaeim1380@gmail.com>
2023-11-25 17:14:45 +0330

    fix(test): fix runner test config

--- src/test/java/calculator/RunnerTest.java
+++ src/test/java/calculator/RunnerTest.java
@@ -4,7 +4,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
-@CucumberOptions(features = "src/test/resources/features", glue = "src/test/java/calculator")
+@CucumberOptions(features = "src/test/resources/features", glue = "calculator")
 @RunWith(Cucumber.class)
 public class RunnerTest {
 
